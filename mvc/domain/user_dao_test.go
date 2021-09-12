10d8474e2e6943366e4e39b123033cfeb6c701dd
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2021-09-12 13:17:22 -0300

    Test User domain

---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,29 @@
+package domain
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserNotUserFound(t *testing.T) {
+	user, err := GetUser(0)
+
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "User 0 was not found", err.Message)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(1)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, 1, user.Id)
+	assert.EqualValues(t, "Geralt", user.FirstName)
+	assert.EqualValues(t, "of Rivia", user.LastName)
+	assert.EqualValues(t, "gr@thewitcher.com", user.Email)
+}
